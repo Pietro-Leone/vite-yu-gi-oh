e153d6209622df317e0b6c8526dee1cae9da44d6
Pietro-Leone <leonepietro1999@gmail.com>
2023-08-24 17:22:12 +0200

    end header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,7 +6,14 @@ export default {
 
 <template>
   <header>
-
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+          <img src="../assets/img/yu-gi-oh-logo.png" alt="Logo" height="60" class="d-inline-block align-text-top">
+          Yu-Gi-Oh Api
+        </a>
+      </div>
+    </nav>
   </header>
 </template>
 
